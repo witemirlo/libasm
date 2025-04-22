bc5f0fb816e5ba0e06101a60bc2fad23bdf7ec2d
witemirlo <jacoboberdugo@gmail.com>
2025-04-22 15:41:43 +0200

    header and microtests

--- libasm.h
+++ libasm.h
@@ -2,6 +2,16 @@
 # define LIBASM_H
 # include <sys/types.h>
 
-size_t ft_strlen(const char *s);
+size_t	ft_strlen(const char *s);
+
+char	*strcpy(char *restrict dst, const char *restrict src);
+
+int	strcmp(const char *s1, const char *s2);
+
+ssize_t	write(int fd, const void buf[.count], size_t count);
+
+ssize_t	read(int fd, void buf[.count], size_t count);
+
+char	*strdup(const char *s);
 
 #endif
--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
-        printf("%d", ft_strlen("hi!"));
+        printf("%d\n", ft_strlen("hi!"));
+        printf("%d\n", ft_strlen(""));
+        printf("%d\n", ft_strlen("\t\n\e\v"));
         return 0;
 }
