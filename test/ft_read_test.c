1f9c097d5a246b4e45346c45e572c4d880a091a4
witemirlo <jacoboberdugo@gmail.com>
2025-04-25 19:26:34 +0200

    wip tests for read

--- test/ft_read_test.c
+++ test/ft_read_test.c
@@ -9,61 +9,78 @@ typedef struct s_pair {
 typedef struct s_data {
 	int     my_errno;
 	ssize_t my_return;
+	char    my_buffer[BUFSIZ];
 	int     og_errno;
 	ssize_t og_return;
+	char    og_buffer[BUFSIZ];
 } t_data;
 
-static t_data test(int fd, void const* buffer, size_t size)
+#define FILE_NAME ".tmp"
+#define NUM 42
+
+static bool check(t_data const* obj)
 {
-	int back = dup(fd);
-	int redir = open("/dev/null", O_WRONLY, 0);
-	t_data ret = {0};
+	if (obj->my_errno != obj->og_errno)
+		return false;
 
-	if (fd != -1)
-		dup2(redir, fd);
+	if (obj->my_return != obj->og_return)
+		return false;
 
-	errno = 0;
-	ret.og_return = write(fd, buffer, size);
-	ret.og_errno = errno;
+	if (strncmp(obj->my_buffer, obj->og_buffer, BUFSIZ) != 0)
+		return false;
 
-	errno = 0;
-	ret.my_return = ft_write(fd, buffer, size);
-	ret.my_errno = errno;
+	return true;
+}
 
-	close(redir);
-	if (back != -1) {
-		dup2(back, fd);
-		close(back);
+static void create_file(void)
+{
+	const int random = open("/dev/random", O_RDONLY, 0);
+	const int fd = open(FILE_NAME, O_CREAT | O_WRONLY, 0644);
+	char      buffer[BUFSIZ];
+
+	for (int i = 0; i < NUM; i++) {
+		read(random, buffer, BUFSIZ);
+		write(fd, buffer, BUFSIZ);
 	}
 
+	close(fd1);
+}
+
+static void test(t_data* obj, int fd1, int fd2, size_t size)
+{
+	errno = 0;
+	obj->og_return = read(fd1, obj->og_buffer, BUFSIZ);
+	obj->og_errno = errno;
+
+	errno = 0;
+	obj->my_return = read(fd1, obj->my_buffer, BUFSIZ);
+	obj->my_errno = errno;
+}
+
+static bool test_file(size_t size)
+{
+	create_file();
+
+	const int og_fd = open(FILE_NAME, O_RDONLY, 0);
+	const int my_fd = open(FILE_NAME, O_RDONLY, 0);
+	t_data    data = {0};
+	bool      ret = true;
+
+	while (true) {
+		test(&data, og_fd, my_fd, size);
+		if (!check(&data)) {
+			ret = false;
+			break;
+		}
+		if (data.my_return < 0)
+			break;
+	}
+	close(og_fd);
+	close(my_fd);
+	unlink(FILE_NAME);
 	return ret;
 }
 
-static t_data test_stdout(void const* buffer, size_t size)
-{
-	return test(1, buffer, size);
-}
-
-static t_data test_stderr(void const* buffer, size_t size)
-{
-	return test(2, buffer, size);
-}
-
-static t_data test_badfd(void const* buffer, size_t size)
-{
-	return test(-1, buffer, size);
-}
-
-static t_data test_file(void const* buffer, size_t size)
-{
-	int fd = open("tmp", O_CREAT | O_WRONLY, 0644);
-
-	t_data ret = test(fd, buffer, size);
-
-	close(fd);
-	unlink("tmp");
-	return ret;
-}
 
 void ft_write_test(void)
 {
@@ -86,12 +103,13 @@ void ft_write_test(void)
 
 	int fd, i, j;
 
+
 	printf("Tests from %s:\n", __func__);
 	for (i = 0; tests[i].str != NULL; i++) {
 		for (j = 0; tests_func[j] != NULL; j++) {
 			ret = (tests_func[j])(tests[i].str, tests[i].size);
 
-			if (ret.og_errno != ret.my_errno || ret.og_return != ret.my_return)
+			if ()
 				fd = 2;
 			else
 				fd = 1;
