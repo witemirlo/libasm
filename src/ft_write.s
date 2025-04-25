083d96e307a618a6c68dc0f55211ea4ed9016dd9
witemirlo <jacoboberdugo@gmail.com>
2025-04-25 13:02:10 +0200

    sync

--- src/ft_write.s
+++ src/ft_write.s
@@ -1,16 +1,30 @@
 .text:
 	global ft_write
+	extern __errno_location
 
 ft_write:
 	push	rbx
 	mov	rbx, rsp
-
-	; call	___error
-	; mov	[rax], 42
+	
+	xor	rax, rax
 
 	mov	rax, 1
 	syscall
 
-_endl:
+	; jmp	_end; debug
+	cmp	rax, 0
+	jb	_end ; no esta haciendo lo que toca
+
+_error:
+	xor	rcx, rcx
+
+	mov	rcx, rax ; save error from syscall
+	neg	rcx
+	
+	call	__errno_location
+	mov	[rax], rcx
+	mov	rax, -1
+
+_end:
 	pop	rbx
 	ret
